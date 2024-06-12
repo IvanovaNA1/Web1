de324b5e1cabe691e779ea49b5c2f5d41ee9b2d8
Надежда <ni74855@gmail.com>
2024-06-12 11:56:49 +0700

    order

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -82,89 +82,222 @@ namespace Web1.Controllers
         public async Task<IActionResult> Orders()
         {
             var orders = await Context.Order
-                .Include(u => u.User)
-                .ToListAsync();
+        .Include(u => u.User)
+        .Where(o => o.IsCompleted == true) // Filter completed orders
+        .OrderBy(o => o.OrderDate) // Sort orders by date ascending
+        .ToListAsync();
 
             var viewModel = new List<OrderViewModel>();
-
             foreach (var order in orders)
             {
-                var orderViewModel = new OrderViewModel
+                if (order.IsCompleted == true)
                 {
-                    OrderID = order.OrderID,
-                    UserName = order.User.UserName,
-                    UserPhone = order.User.UserPhone,
-                    OrderDate = order.OrderDate,
-                    OrderProductsList = await Context.OrderProducts.Where(sl => sl.OrderID == order.OrderID).ToListAsync(),
-                    OrderServicesList = await Context.OrderServices.Where(sl => sl.OrderID == order.OrderID).ToListAsync()
+                    var orderViewModel = new OrderViewModel
+                    {
+                        OrderID = order.OrderID,
+                        UserName = order.User.UserName,
+                        UserPhone = order.User.UserPhone,
+                        OrderDate = order.OrderDate,
+                        OrderProductsList = await Context.OrderProducts.Where(sl => sl.OrderID == order.OrderID).ToListAsync(),
+                        OrderServicesList = await Context.OrderServices.Where(sl => sl.OrderID == order.OrderID).ToListAsync()
 
-                };
-
-                viewModel.Add(orderViewModel);
+                    };
+                    viewModel.Add(orderViewModel);
+                }
             }
-
             return View(viewModel);
         }
-        //public async Task FindOrder()
-        //{
-        //    var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
-        //    var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
-
-        //    if (user != null)
-        //    {
-        //        var userOrders = await Context.Order
-        //                                      .Where(o => o.UserID == user.UserID)
-        //                                      .ToListAsync();
-        //        ViewBag.UserOrders = userOrders.Count;
-        //    }
-        //    else
-        //    {
-        //        ViewBag.UserOrders = 0;
-        //    }
-        //}
         [HttpPost]
         public async Task<IActionResult> AddToCart(string productName, double unitPrice)
         {
             var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
             var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
+            var order = await Context.Order
+                .Include(o => o.OrderProducts)
+                .FirstOrDefaultAsync(o => o.UserID == user.UserID && !o.IsCompleted);
+            if (order == null)
+            {
+                order = new Order
+                {
+                    UserID = user.UserID,
+                    DeliveryID = 1,
+                    OrderDate = DateTime.Now,
+                    IsCompleted = false
+                };
 
-            var order = new Order
-            {
-                UserID = user.UserID,
-                DeliveryID = 1, 
-                OrderDate = DateTime.Now
-            };
-            try
-            {
-                Context.Order.Add(order);
-                Context.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError("", "error in order " + ex.Message);
-            }
-            var model = new OrderProducts
-            {
-                ProductName = productName,
-                ProductCount = 1,
-                OrderID = order.OrderID,
-            };
-            if (ModelState.IsValid)
-            {
                 try
                 {
-                    Context.OrderProducts.Add(model);
+                    Context.Order.Add(order);
                     Context.SaveChanges();
-                    //await FindOrder();
-                    return RedirectToAction("Order", "Home");
                 }
                 catch (Exception ex)
                 {
-                    ModelState.AddModelError("", "error in orderProducts " + ex.Message);
+                    ModelState.AddModelError("", "error in order " + ex.Message);
+                    return View();
                 }
             }
+
+            if (order != null && order.OrderProducts != null)
+            {
+                var existingProduct = order.OrderProducts.FirstOrDefault(op => op.ProductName == productName);
+                if (existingProduct != null)
+                {
+                    existingProduct.ProductCount += 1;
+                }
+                else
+                {
+                    var orderProduct = new OrderProducts
+                    {
+                        ProductName = productName,
+                        ProductCount = 1,
+                        OrderID = order.OrderID,
+                    };
+
+                    order.OrderProducts.Add(orderProduct);
+                }
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        Context.SaveChanges();
+                        ViewBag.ProductCount = order.OrderProducts.Sum(op => op.ProductCount);
+                        return RedirectToAction("Order", "Home");
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", "error in orderProducts " + ex.Message);
+                    }
+                }
+            }
+            
             return View();
         }
+        public async Task<IActionResult> Order()
+        {
+            var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
+            var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
 
+            var orders = await Context.Order
+                .Include(o => o.OrderProducts)
+                .Where(o => o.UserID == user.UserID && !o.IsCompleted)
+                .ToListAsync();
+
+            var orderProducts = orders.SelectMany(o => o.OrderProducts)
+                                  .Select(op => new
+                                  {
+                                      Product = new Product
+                                      {
+                                          ProductName = op.ProductName,
+                                          ProductPrice = Context.Product.FirstOrDefault(p => p.ProductName == op.ProductName).ProductPrice
+                                      },
+                                      ProductCount = op.ProductCount
+                                  })
+                                  .ToList();
+
+            ViewBag.ProductCountList = orderProducts.ToDictionary(op => op.Product.ProductName, p => p.ProductCount);
+            var products = orderProducts.Select(op => op.Product).ToList();
+            decimal totalAmount = orderProducts.Sum(op => op.Product.ProductPrice * op.ProductCount);
+            ViewBag.TotalAmount = totalAmount;
+
+            return View(products);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromCart(string productName)
+        {
+            var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
+            var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
+
+            var order = await Context.Order
+                .Include(o => o.OrderProducts)
+                .FirstOrDefaultAsync(o => o.UserID == user.UserID && !o.IsCompleted);
+
+            if (order != null && order.OrderProducts != null)
+            {
+                var productToRemove = order.OrderProducts.FirstOrDefault(op => op.ProductName == productName);
+                if (productToRemove != null)
+                {
+                    order.OrderProducts.Remove(productToRemove);
+                    try
+                    {
+                        await Context.SaveChangesAsync();
+                        return RedirectToAction("Order", "Home");
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", "error in removing product " + ex.Message);
+                    }
+                }
+            }
+
+            return RedirectToAction("Order", "Home");
+        }
+        [HttpPost]
+        public async Task<IActionResult> PlaceOrder()
+        {
+            var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
+            var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
+
+            var order = await Context.Order
+                .Include(o => o.OrderProducts)
+                .FirstOrDefaultAsync(o => o.UserID == user.UserID && !o.IsCompleted);
+            if (order != null)
+            {
+                order.IsCompleted = true;
+                order.OrderDate = DateTime.Now;
+                Context.SaveChanges();
+
+                return RedirectToAction("Payment");
+            }
+            return RedirectToAction("Order");
+        }
+        [HttpPost]
+        public async Task<IActionResult> IncreaseProductCount(string productName)
+        {
+            var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
+            var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
+
+            var order = await Context.Order
+                .Include(o => o.OrderProducts)
+                .FirstOrDefaultAsync(o => o.UserID == user.UserID && !o.IsCompleted);
+
+            if (order != null && order.OrderProducts != null)
+            {
+                var existingProduct = order.OrderProducts.FirstOrDefault(op => op.ProductName == productName);
+                if (existingProduct != null)
+                {
+                    existingProduct.ProductCount += 1;
+                    await Context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToAction("Order", "Home");
+        }
+        [HttpPost]
+        public async Task<IActionResult> DecreaseProductCount(string productName)
+        {
+            var userName = User.FindFirstValue(ClaimsIdentity.DefaultNameClaimType);
+            var user = await Context.User.FirstOrDefaultAsync(u => u.UserLogin == userName);
+
+            var order = await Context.Order
+                .Include(o => o.OrderProducts)
+                .FirstOrDefaultAsync(o => o.UserID == user.UserID && !o.IsCompleted);
+
+            if (order != null && order.OrderProducts != null)
+            {
+                var existingProduct = order.OrderProducts.FirstOrDefault(op => op.ProductName == productName);
+                if (existingProduct != null && existingProduct.ProductCount > 1)
+                {
+                    existingProduct.ProductCount -= 1;
+                    await Context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToAction("Order", "Home");
+        }
+        public IActionResult Payment()
+        {
+            return View();
+        }
     }
 }
--- Models/DBModels/OrderProducts.cs
+++ Models/DBModels/OrderProducts.cs
@@ -5,9 +5,10 @@ namespace Web1.Models.DBModels
 {
     public class OrderProducts
     {
-        [Key]public int OrderProductsID { get; set; }
+        [Key] public int OrderProductsID { get; set; }
         public int OrderID { get; set; }
         public string ProductName { get; set; }
         public int ProductCount { get; set; }
+        public Order Order { get; set; }
     }
 }
